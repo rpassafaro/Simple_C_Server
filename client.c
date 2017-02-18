79a65d2a9c6db1d6e4931ebbf32e5bb6554015ac
robert passafaro <rpassafa@e1z3r1p5.42.us.org>
2017-02-17 19:11:32 -0800

    normed

--- client.c
+++ client.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/17 19:08:47 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/17 19:08:50 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main()
+int	main(void)
 {
-    int sockfd;
-    char recvline[100];
-    struct sockaddr_in servaddr;
+	int					sockfd;
+	char				recvline[100];
+	struct sockaddr_in	servaddr;
 
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(22000);
-    inet_pton(AF_INET,"127.0.0.1",&(servaddr.sin_addr));
-    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
-    write(sockfd,"ping",5);
-    read(sockfd,recvline,100);
-    write(1,recvline,11);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(22000);
+	inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
+	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	write(sockfd, "ping", 5);
+	read(sockfd, recvline, 100);
+	write(1, recvline, 11);
 }
--- server.c
+++ server.c
@@ -1,28 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/17 19:11:10 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/17 19:11:16 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
-//#include <string.h>
 #include <unistd.h>
 
-int main()
+int	main(void)
 {
+	char				str[100];
+	int					listen_fd;
+	int					comm_fd;
+	struct sockaddr_in	servaddr;
 
-    char str[100];
-    int listen_fd;
-	int comm_fd;
-    struct sockaddr_in servaddr;
-
-    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(22000);
-    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    listen(listen_fd, 10);
-    while(1)
-    {
-		comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-        read(comm_fd,str,100);
-        write(comm_fd, "pong\npong\n", 11);
-    }
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+	servaddr.sin_port = htons(22000);
+	bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	listen(listen_fd, 10);
+	while (1)
+	{
+		comm_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
+		read(comm_fd, str, 100);
+		write(comm_fd, "pong\npong\n", 11);
+	}
 }
